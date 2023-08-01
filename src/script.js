57e8367b133e7273acf26533e554334a8709149f
Riley <riley.childs@yahoo.com>
2023-08-01 16:17:57 -0400

    Pulled data from script.js

---
+++ src/script.js
@@ -0,0 +1,27 @@
+// Delete once data is on server
+import data from './data.json';
+console.log(data)
+// ///////////////////////////////////
+
+
+
+// async function fetchData() {
+//     try {
+//       const response = await fetch('data.json');
+  
+//       if (!response.ok) {
+//         throw new Error('Network response was not ok');
+//       }
+  
+//       const data = await response.json();
+//       // Use the fetched data here
+//       console.log(data);
+//     } catch (error) {
+//       // Handle errors
+//       console.error('Fetch error:', error);
+//     }
+//   }
+  
+//   // Call the function to retrieve the data
+//   fetchData();
+  

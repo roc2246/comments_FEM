a1dcce9510b847f771a41f7f315cd27c22d403b0
Riley <riley.childs@yahoo.com>
2023-08-03 12:41:51 -0400

    Refactored componenets

--- dist/script.75da7f30.js
+++ dist/script.75da7f30.js
@@ -396,7 +396,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58527" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58774" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
